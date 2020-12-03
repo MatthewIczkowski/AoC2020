0e99ca277cba2b4c0c7853af18d3c1e6c9342ab8
MatthewIczkowski <iczkom13@gmail.com>
2020-12-02 22:21:47 -0800

    Cleanup

---
+++ projects/day3/day3.py
@@ -0,0 +1,53 @@
+#Day 3: Toboggan Trajectory
+
+from projects.helpers import io
+
+arr = []
+
+def add_to_arr(line):
+    try:
+        arr_item = line
+        arr.append(arr_item)
+    except ValueError:
+        return
+
+io.read_file_by_newline("day3.txt", add_to_arr)
+
+def is_tree(pos):
+    if pos == "#":
+        return 1
+    else:
+        return 0
+
+#variables:
+col = 0
+row = 0
+tree_count = 0
+
+#restricted by movement over 3 columns and 1 row down
+#step 1: start at arr[0][0] in upper lefthand corner of map
+
+for row in range(0,len(arr)-1):
+    # step 2: add 3 to column, add 1 to row
+    col += 3
+    row += 1
+    # Step 3: once last col is reached, subtract by number of columns to wrap back to beginning of row
+    # step 4: check if current position is '#' or '.']
+    if row >= len(arr)-1:
+        print("No more rows, current row: ", row)
+        break
+    elif col >= len(arr[row]):
+        #print("Column attempt: ", col)
+        col = col - len(arr[row])
+        tree_count += is_tree(arr[row][col])
+        #print("Column > 32, column = ", col)
+        #break
+    else:
+        tree_count += is_tree(arr[row][col])
+    #move up 1 row to account for forloop increment
+    #row =- 1
+
+print("Tree count: ", tree_count)
+
+
+
