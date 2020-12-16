b4dcacfb17f2ad7d971e382618ed55a97f1cff89
MatthewIczkowski <iczkom13@gmail.com>
2020-12-16 16:37:20 -0500

    Day 4 Part 2 solution update

--- projects/day4/day4.py
+++ projects/day4/day4.py
@@ -11,7 +11,7 @@ def add_to_rows(line):
     except ValueError:
         return
 
-io.read_file_by_newline("day4_unit_test2.txt", add_to_rows)
+io.read_file_by_newline("day4.txt", add_to_rows)
 
 #Function to create array of people from rows
 def parse_people(rows):
@@ -29,10 +29,10 @@ def parse_people(rows):
             persons.append('')
             index += 1
 
-    for person in persons:
-        print(person)
+    #for person in persons:
+        #print(person)
 
-    print("Number of peoeple in persons: ", len(persons))
+    #print("Number of peoeple in persons: ", len(persons))
     return persons
 
 #Function to create dictonary of attributes from person
@@ -58,19 +58,38 @@ def is_valid_passport_1(passport):
 #Function to test if passport with all required fields meets the data validation criteria
 def is_valid_passport_2(passport):
 
+    hcl_chars = ['#', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
+    ecl_types = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+
     if int(passport['byr']) < 1920 or int(passport['byr']) > 2020:
+        #print(passport, '\n', "Invalid passport, 'byr' out of range. 'byr': ", passport['byr'])
         return 0
     elif int(passport['iyr']) < 2010 or int(passport['iyr']) > 2020:
+        #print(passport, '\n', "Invalid passport, 'iyr' out of range. 'iyr': ", passport['iyr'])
         return 0
     elif int(passport['eyr']) < 2020 or int(passport['eyr']) > 2030:
+        #print(passport, '\n', "Invalid passport, 'eyr' out of range. 'eyr': ", passport['eyr'])
         return 0
-    elif 'cm' in passport['hgt'] and int(passport['hgt'].strip('cm')) < 150 or int(passport['hgt'].strip('cm')) > 193:
+    elif 'cm' not in passport['hgt'] and 'in' not in passport['hgt']:
+        #print(passport, '\n', "Invalid passport, 'hgt' does not contain 'cm' or 'in'. 'hgt': ", passport['hgt'])
         return 0
-    elif 'in' in passport['hgt'] and int(passport['hgt'].strip('in')) < 59 or int(passport['hgt'].strip('in')) > 76:
+    elif 'cm' in passport['hgt'] and (int(passport['hgt'].strip('cmin')) < 150 or int(passport['hgt'].strip('cmin')) > 193):
+        #print(passport, '\n', "Invalid passport, 'hgt' out of cm range. 'hgt': ", passport['hgt'])
         return 0
-    elif 'cm' not in passport['hgt'] or 'in' not in passport['hgt']:
+    elif 'in' in passport['hgt'] and (int(passport['hgt'].strip('cmin')) < 59 or int(passport['hgt'].strip('cmin')) > 76):
+        #print(passport, '\n', "Invalid passport, 'hgt' out of in range. 'hgt': ", passport['hgt'])
+        return 0
+    elif '#' not in passport['hcl'] or len(passport['hcl']) != 7 or any(x not in hcl_chars for x in passport['hcl']):
+        #print(passport, '\n', "Invalid passport, 'hcl' does not match required format. 'hcl': ", passport['hcl'])
+        return 0
+    elif passport['ecl'] not in ecl_types:
+        # print(passport, '\n', "Invalid passport, 'ecl' does not match value in 'ecl' list. 'ecl': ", passport['ecl'])
+        return 0
+    elif len(passport['pid']) != 9 or not passport['pid'].isdigit():
+        #print(passport, '\n', "Invalid passport, 'pid' does not match required format. 'pid': ", passport['pid'])
         return 0
     else:
+        #print("Valid passport: ", '\n', passport)
         return 1
 
 persons = parse_people(rows)
@@ -82,10 +101,9 @@ for person in persons:
         num_valid_1 += 1
         #If passport has all the required fields then see if passport has valid data
         passdict = parse_person(person)
-        print(passdict)
         num_valid_2 += is_valid_passport_2(passdict)
 
-#print("Number of Valid Passports (logic - is_valid_passport_1): ", num_valid_1)
+print("Number of Valid Passports (logic - is_valid_passport_1): ", num_valid_1)
 print("Number of Valid Passports (logic - is_valid_passport_2): ", num_valid_2)
 
 
