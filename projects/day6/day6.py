afc0b4885ae45a1ffee16dc45e6cd5a1ac6ed82f
MatthewIczkowski <iczkom13@gmail.com>
2020-12-18 09:03:38 -0500

    Day 6 Part 2 Solution update

--- projects/day6/day6.py
+++ projects/day6/day6.py
@@ -1,5 +1,6 @@
 # Day 6 : Custom Customs
 from projects.helpers import io
+import string
 
 dataset = []
 
@@ -21,7 +22,7 @@ def parse_people(rows):
         if len(row) >= 1:
             #print(index)
             row=row.rstrip()
-            persons[index] = persons[index] + row
+            persons[index] = persons[index] + ' ' + row
             persons[index] = persons[index].strip()
         else:
             persons.append('')
@@ -34,15 +35,44 @@ def parse_people(rows):
     return persons
 
 def find_unique(group):
+    group = group.replace(' ', '')
     is_unique = []
     for letter in group:
         if letter not in is_unique:
             is_unique.append(letter)
     return len(is_unique)
 
-forms = parse_people(dataset)
-total = 0
-for form in forms:
-    total += find_unique(form)
+def find_common(group):
+    people = 1
+    for char in group:
+        if char == ' ':
+            people += 1
+    groups = group.split()
 
-print("total = ", total)
+    #create dictionary of lowercase alphabet
+    alphabet = list(string.ascii_lowercase)
+    alphabet_count = dict.fromkeys(alphabet, 0)
+
+    count_common = 0
+    if people == 1:
+        return len(groups[0])
+    else:
+        for person in groups:
+            for letter in person:
+                alphabet_count[letter] += 1
+
+        for value in alphabet_count.values():
+            if value == people:
+                count_common += 1
+        return count_common
+
+forms = parse_people(dataset)
+total_unique = 0
+total_common = 0
+for form in forms:
+    total_unique += find_unique(form)
+    total_common += find_common(form)
+
+print("total_unique = ", total_unique)
+print("total_common = ", total_common)
+
